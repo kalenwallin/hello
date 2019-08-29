24a3e2662daa3aaf185a043c16b34cfad8eb2d8f
jsinica <jmsinica@gmail.com>
2019-08-29 12:09:19 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -5,7 +5,8 @@ int main(int argc, char **argv) {
 
   printf("Kalen Wallin\n");
   printf("Computer Science\n");
-  
+  printf("Jarred Sinica\n");
+  printf("software Engineering\n");
   return 0;
 
 }
